d7cb375532cdfc076ffa009454726b9ab02c0da7
Ignacy Sawa <bd1cb5c6@opayq.com>
2023-06-08 16:51:58 +0200

    Added pseudocode for master script running the whole project

---
+++ playtrough.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# General script for the whole project.
+# It runs all project's tasks one by one.
+
+# Prompt user for the vault password
+
+# Create and/or open a vault
+
+# Generate an SSH key for the project
+
+# Upload the key to AWS
+
+# Ask user for the region, instance type, inform about defaults
+
+# Set up a VM on AWS using Terraform
+
+# Test that VM works
+
+# Provision the VM for operations in preparation for Jenkins
+# installation
+
+# Install Jenkins on the VM
+
+# Configure Jenkins on the VM
+
+# Have Jenkins deploy the app container to AWS
+
+# Commit a change to the project's code repo (modify code of the app)
+
+# Confirm that Jenkins pushed a new version of the app's container to
+# the cloud.
+
+# Congratulations, it works!
