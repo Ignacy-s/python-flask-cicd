151821a59a6836b787705d95f1b2e78c44fd2f00
Ignacy Sawa <bd1cb5c6@opayq.com>
2023-06-10 14:54:42 +0200

    Add an usage() and main() to playtrough.sh

--- playtrough.sh
+++ playtrough.sh
@@ -2,33 +2,65 @@
 
 # General script for the whole project.
 # It runs all project's tasks one by one.
+# Requisite: Script is run in the project's root directory else there
+# will be fun.
 
-# Prompt user for the vault password
 
-# Create and/or open a vault
+# VARIABLES
+VAULT_PASSPHRASE=""
 
-# Generate an SSH key for the project
+# FUNCTIONS
+function usage() {
+cat <<EOF >&2
+Usage: ${0} X[-Y]
+Where X is the step you want to start at (0 to start from the
+beginning), and Y is the step you want to end with. Y is optional, if
+not submitted, the script will run until the end.
+EOF
+}
 
-# Upload the key to AWS
 
-# Ask user for the region, instance type, inform about defaults
+# MAIN FUNCTION
+main(){
+  # Prompt user for the vault password
+  #TODO allow alternative, maybe read from file
+  read -s -p 'Enter the vault password: ' VAULT_PASSPHRASE
+  export VAULT_PASSPHRASE
 
-# Set up a VM on AWS using Terraform
+  # Create and/or open a vault
+  #TODO For now there will be an error if we try to re-create a vault or
+  #open an existing vault if it's already open.
 
-# Test that VM works
+  # Check if a vault exists and can be opened with the
 
-# Provision the VM for operations in preparation for Jenkins
-# installation
 
-# Install Jenkins on the VM
+  # Generate an SSH key for the project
 
-# Configure Jenkins on the VM
+  # Upload the key to AWS
 
-# Have Jenkins deploy the app container to AWS
+  # Ask user for the region, instance type, inform about defaults
 
-# Commit a change to the project's code repo (modify code of the app)
+  # Set up a VM on AWS using Terraform
 
-# Confirm that Jenkins pushed a new version of the app's container to
-# the cloud.
+  # Test that VM works
 
-# Congratulations, it works!
+  # Provision the VM for operations in preparation for Jenkins
+  # installation
+
+  # Install Jenkins on the VM
+
+  # Configure Jenkins on the VM
+
+  # Have Jenkins deploy the app container to AWS
+
+  # Commit a change to the project's code repo (modify code of the app)
+
+  # Confirm that Jenkins pushed a new version of the app's container to
+  # the cloud.
+
+  # Congratulations, it works!
+  return 0
+}
+
+main "${@}"
+exit $?
