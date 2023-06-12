8fb7b4d28c63e5a1b8a099498d7cb66a7872cc91
Ignacy Sawa <bd1cb5c6@opayq.com>
2023-06-12 11:09:07 +0200

    Move actions in playtrough.sh into steps
    Steps are an attempt to re-create a fall trough switch statement in
    Bash.
    This allows for easier testing of the project, starting from actions
    in the middle or not going all the way trough.

--- playtrough.sh
+++ playtrough.sh
@@ -22,45 +22,102 @@ EOF
 
 # MAIN FUNCTION
 main(){
+
+  if (( ${#} < 1 )); then
+    die "You didn't provide the first step argument."
+  fi
+  
+  START_POINT="${1}"
+  LAST_POINT="${2:-999}"
+
+
+  STEP=1
   # Prompt user for the vault password
   #TODO allow alternative, maybe read from file
-  read -s -p 'Enter the vault password: ' VAULT_PASSPHRASE
-  export VAULT_PASSPHRASE
+  if [[ ( START_POINT -le STEP ) && ( LAST_POINT -ge STEP ) ]]; then
+    read -s -p 'Enter the vault password: ' VAULT_PASSPHRASE
+    export VAULT_PASSPHRASE
+  fi
 
+  STEP=2
   # Create and/or open a vault
   #TODO For now there will be an error if we try to re-create a vault or
   #open an existing vault if it's already open.
+  if [[ ( START_POINT -le STEP ) && ( LAST_POINT -ge STEP ) ]]; then
+    echo "Performing step ${STEP}."
+  fi
 
-  # Check if a vault exists and can be opened with the
-
-
+  STEP=3
   # Generate an SSH key for the project
+  if [[ ( START_POINT -le STEP ) && ( LAST_POINT -ge STEP ) ]]; then
+    echo "Performing step ${STEP}."
+  fi
 
+  STEP=4
   # Upload the key to AWS
+  if [[ ( START_POINT -le STEP ) && ( LAST_POINT -ge STEP ) ]]; then
+    echo "Performing step ${STEP}."
+  fi
 
+  STEP=5
   # Ask user for the region, instance type, inform about defaults
+  if [[ ( START_POINT -le STEP ) && ( LAST_POINT -ge STEP ) ]]; then
+    echo "Performing step ${STEP}."
+  fi
 
+  STEP=6
   # Set up a VM on AWS using Terraform
+  if [[ ( START_POINT -le STEP ) && ( LAST_POINT -ge STEP ) ]]; then
+    echo "Performing step ${STEP}."
+  fi
 
+  STEP=7
   # Test that VM works
+  if [[ ( START_POINT -le STEP ) && ( LAST_POINT -ge STEP ) ]]; then
+    echo "Performing step ${STEP}."
+  fi
 
+  STEP=8
   # Provision the VM for operations in preparation for Jenkins
   # installation
+  if [[ ( START_POINT -le STEP ) && ( LAST_POINT -ge STEP ) ]]; then
+    echo "Performing step ${STEP}."
+  fi
 
+  STEP=9
   # Install Jenkins on the VM
+  if [[ ( START_POINT -le STEP ) && ( LAST_POINT -ge STEP ) ]]; then
+    echo "Performing step ${STEP}."
+  fi
 
+  STEP=10
   # Configure Jenkins on the VM
+  if [[ ( START_POINT -le STEP ) && ( LAST_POINT -ge STEP ) ]]; then
+    echo "Performing step ${STEP}."
+  fi
 
+  STEP=11
   # Have Jenkins deploy the app container to AWS
+  if [[ ( START_POINT -le STEP ) && ( LAST_POINT -ge STEP ) ]]; then
+    echo "Performing step ${STEP}."
+  fi
 
+  STEP=12
   # Commit a change to the project's code repo (modify code of the app)
+  if [[ ( START_POINT -le STEP ) && ( LAST_POINT -ge STEP ) ]]; then
+    echo "Performing step ${STEP}."
+  fi
 
+  STEP=13
   # Confirm that Jenkins pushed a new version of the app's container to
   # the cloud.
+  if [[ ( START_POINT -le STEP ) && ( LAST_POINT -ge STEP ) ]]; then
+    echo "Performing step ${STEP}."
+  fi
 
   # Congratulations, it works!
   return 0
 }
 
 main "${@}"
-exit $?
+exit ${?}
