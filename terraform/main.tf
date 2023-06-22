6694bd51966551e19f89fdeb952ecc4c7cb69bfd
Ignacy Sawa <bd1cb5c6@opayq.com>
2023-06-22 18:22:29 +0200

    Add routing table to main.tf, fixing problem with accessing instance
    Added an internet gateway for public subnet, routing table containing
    how to access 0.0.0.0/0 (all internet) for public subnet and routing
    table association.

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,10 +13,36 @@ resource "aws_vpc" "jenkins" {
 resource "aws_subnet" "jenkins" {
   vpc_id                  = aws_vpc.jenkins.id
   cidr_block              = "10.0.1.0/24"
-
+  map_public_ip_on_launch = true
   tags = {
     Name = "JenkinsSubnet"
   }
+}
+
+resource "aws_internet_gateway" "jenkins" {
+  vpc_id = aws_vpc.jenkins.id
+
+  tags = {
+    Name = "JenkinsIGW"
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.jenkins.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.jenkins.id
+  }
+
+  tags = {
+    Name = "Public Subnet Routing Table"
+  }
+}
+
+resource "aws_route_table_association" "rt_associate_public" {
+  subnet_id = aws_subnet.jenkins.id
+  route_table_id = aws_route_table.route_table.id
 }
 
 resource "aws_security_group" "jenkins" {
@@ -50,12 +76,20 @@ resource "aws_security_group" "jenkins" {
   }
 }
 
+resource "aws_key_pair" "jenkins-key" {
+  key_name      = "cicd-project-key"
+  public_key    = file("../vault/my_ssh_key_mount/id_25519_aws_flaskcicd.pub")
+}
+
+
 resource "aws_instance" "jenkins" {
-  ami           = data.aws_ami.alma_linux_9.id
+  ami           = "ami-04e4606740c9c9381"
+#  ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
 
-#  key_name      = "your_key_pair_name"
+  key_name      = aws_key_pair.jenkins-key.key_name
   subnet_id     = aws_subnet.jenkins.id
+  associate_public_ip_address = true
 
   vpc_security_group_ids = [
     aws_security_group.jenkins.id
